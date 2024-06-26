54ab496f5c8d53ed332889303aa0f095b489686b
ole <ou8zz@sina.com>
2024-06-26 10:54:16 +0800

    feat: build docker to aliyun

--- build.sh
+++ build.sh
@@ -8,3 +8,8 @@ docker build -f Dockerfile -t registry.cn-shanghai.aliyuncs.com/ou88zz/filebrows
 
 # docker push ou88zz/filebrowser:$1
 docker push registry.cn-shanghai.aliyuncs.com/ou88zz/filebrowser:$1
+
+git add .
+git commit -m "auto submit $1"
+git push
+echo $(date "+%Y-%m-%d %H:%M:%S")
