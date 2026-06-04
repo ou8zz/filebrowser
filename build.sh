9e6c637bd816f20f0d9a74ac106498806b7951f8
ole <ou8zz@sina.com>
2026-06-04 17:09:48 +0800

    auto submit 1.8.2

--- build.sh
+++ build.sh
@@ -35,7 +35,8 @@ build_frontend
 build_backend
 
 echo 'version:'$1
-docker build -f Dockerfile.s6 -t registry.cn-shanghai.aliyuncs.com/ou88zz/filebrowser:$1 .
+docker build -t registry.cn-shanghai.aliyuncs.com/ou88zz/filebrowser:$1 .
+# docker build -f Dockerfile.s6 -t registry.cn-shanghai.aliyuncs.com/ou88zz/filebrowser:$1 .
 docker push registry.cn-shanghai.aliyuncs.com/ou88zz/filebrowser:$1
 # docker build -t ou88zz/filebrowser:$1 .
 # docker push ou88zz/filebrowser:$1
